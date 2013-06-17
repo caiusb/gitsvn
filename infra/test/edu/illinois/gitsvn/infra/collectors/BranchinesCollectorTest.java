6901f0bbf74421c3542b1f73655f56bd008d982a
Caius Brindescu <brind@illinois.edu>
2013-06-17 17:22:59 -0500

    Added a test for the BranchinessCollector

---
+++ infra/test/edu/illinois/gitsvn/infra/collectors/BranchinesCollectorTest.java
@@ -0,0 +1,55 @@
+package edu.illinois.gitsvn.infra.collectors;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.MergeResult;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.gitective.tests.GitTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BranchinesCollectorTest extends GitTestCase {
+	
+	private BranchinesCollector filter;
+	private RevWalk revWalk;
+	private Repository repository;
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+
+		repository = Git.open(testRepo).getRepository();
+		filter = new BranchinesCollector();
+		filter.setRepository(repository);
+		revWalk = new RevWalk(repository);
+	}
+	
+	@Test
+	public void testNormalCommit() throws Exception {
+		RevCommit cmit = add("f","content");
+		
+		boolean result = filter.include(revWalk, cmit);
+		assertTrue(result);
+		assertEquals(0, filter.getMaturity(), 0.01);
+	}
+	
+	@Test
+	public void testMergeCommit() throws Exception {
+		add("f","content");
+		branch("test-branch");
+		checkout("test-branch");
+		add("g","some other content");
+		checkout("master");
+		add("f","some different content");
+		MergeResult mergeResult = merge("test-branch");
+		ObjectId newHead = mergeResult.getNewHead();
+		RevCommit mergeCommit = revWalk.parseCommit(newHead);
+		
+		boolean result = filter.include(revWalk, mergeCommit);
+		assertTrue(result);
+		assertEquals(1, filter.getMaturity(), 0.01);
+	}
+
+}
