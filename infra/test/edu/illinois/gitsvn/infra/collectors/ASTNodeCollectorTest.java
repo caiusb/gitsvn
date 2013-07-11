93fb8eaccbf8833acd167a4c6a634606612260a6
Caius Brindescu <brind@illinois.edu>
2013-07-10 19:03:15 -0500

    Added a test for the problematic commit
    This tests takes a very long time and uses a large amount of
    memory (6GB minimum so it doesn't run out of memory).
    
    So far, I haven't been able to run it completely, but I will
    try harder.

--- infra/test/edu/illinois/gitsvn/infra/collectors/ASTNodeCollectorTest.java
+++ infra/test/edu/illinois/gitsvn/infra/collectors/ASTNodeCollectorTest.java
@@ -1,9 +1,12 @@
 package edu.illinois.gitsvn.infra.collectors;
 
+import java.io.File;
+
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.gitective.core.CommitUtils;
 import org.gitective.tests.GitTestCase;
 import org.junit.After;
 import org.junit.Before;
@@ -46,6 +49,16 @@ public class ASTNodeCollectorTest extends GitTestCase{
 		
 		collector.include(walker, commit);
 		assertEquals("1",collector.getDataForCommit());
+	}
+	
+	@Test
+	public void testProblemCommitInJDTCore() throws Exception {
+		repository = Git.open(new File("../../svnToGitRepos/eclipse.jdt.core")).getRepository();
+		collector.setRepository(repository);
+		walker = new RevWalk(repository);
+		
+		RevCommit problemCommit = CommitUtils.getCommit(repository, "968a44abcc71c95b90a66b2bfce384792751e997");
+		collector.include(walker, problemCommit);
 	}
 
 }
