9c5bd71c4de812e55b27004245968a1359fc6e5a
Caius Brindescu <brind@illinois.edu>
2013-07-24 17:16:40 -0500

    The immediate writer
    It writes as you give it data. This way, you don't loose
    anything if something goes wrong in the middle of an
    analysis.

---
+++ infra/src/edu/illinois/gitsvn/infra/csv/ImmediateCSVWriter.java
@@ -0,0 +1,56 @@
+package edu.illinois.gitsvn.infra.csv;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ImmediateCSVWriter extends CSVCollector {
+	
+	private Path csvFilePath;
+
+	public ImmediateCSVWriter(String csvFile) {
+		csvFilePath = Paths.get(csvFile);
+		try {
+			Files.deleteIfExists(csvFilePath);
+			Files.createFile(csvFilePath);
+		} catch (IOException e) {
+		}
+	}
+	
+	@Override
+	public void addHeader(List<String> headers) {
+		super.addHeader(headers);
+		byte[] bytes = getBytesForList(headers);
+		try {
+			Files.write(csvFilePath, bytes);
+		} catch (IOException e) {
+		}
+	}
+	
+	@Override
+	public void addRow(List<String> row) {
+		super.addRow(row);
+		byte[] bytes = getBytesForList(row);
+		try {
+			Files.write(csvFilePath, bytes);
+		} catch (IOException e) {
+		}
+	}
+
+	private byte[] getBytesForList(List<String> listOfThings) {
+		StringBuffer stringBuffer = new StringBuffer();
+		for (String header : listOfThings) {
+			stringBuffer.append("\"");
+			stringBuffer.append(header);
+			stringBuffer.append(",");
+			stringBuffer.append("\"");
+		}
+		stringBuffer.deleteCharAt(stringBuffer.lastIndexOf(","));
+		
+		byte[] bytes = stringBuffer.toString().getBytes();
+		return bytes;
+	}
+
+}
