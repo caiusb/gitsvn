8a1ef9c6b9d92121add816042cf1e5396d7371f7
Caius Brindescu <brind@illinois.edu>
2013-07-25 12:19:22 -0500

    Converted the runner into a paramerized test
    So the whole thing does not stop if one of the projects
    blows up. So far it's really ugly, so it will need a bit of
    refactorings to get it working smoothly.

--- infra/src/edu/illinois/gitsvn/analysis/launchers/SvnToGitRepoAnalysisLauncher.java
+++ infra/src/edu/illinois/gitsvn/analysis/launchers/SvnToGitRepoAnalysisLauncher.java
@@ -1,14 +1,22 @@
 package edu.illinois.gitsvn.analysis.launchers;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import edu.illinois.gitsvn.analysis.CutoffGenericAnalysis;
 import edu.illinois.gitsvn.infra.AnalysisConfiguration;
 import edu.illinois.gitsvn.infra.AnalysisLauncher;
 
+@RunWith(Parameterized.class)
 public class SvnToGitRepoAnalysisLauncher extends AnalysisLauncher {
+
+	private AnalysisLauncher launcher;
 
 	@Override
 	protected void populateWithConfigurations(List<AnalysisConfiguration> configurations) {
@@ -33,12 +41,52 @@ public class SvnToGitRepoAnalysisLauncher extends AnalysisLauncher {
 
 	}
 
-	public static void main(String[] args) throws Exception {
-		new SvnToGitRepoAnalysisLauncher().run();
+//	public static void main(String[] args) throws Exception {
+//		new SvnToGitRepoAnalysisLauncher().run();
+//	}
+//	
+//	@Test
+//	public void test() throws Exception {
+//		new SvnToGitRepoAnalysisLauncher().run();
+//	}
+	
+	public SvnToGitRepoAnalysisLauncher(final String path, final String name, final int cutoffTime) {
+		launcher = new AnalysisLauncher() {
+			
+			@Override
+			protected void populateWithConfigurations(List<AnalysisConfiguration> configurations) {
+				configurations.add(new CutoffGenericAnalysis(path, name, cutoffTime));
+			}
+		};
+	}
+	
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] data = new Object[][] {
+				{"../../svnToGitRepos/junit", "JUnit", 1231818542},
+				{"../../svnToGitRepos/Arduino", "Arduino", 1284741802},
+				{"../../svnToGitRepos/cyclopsgroup", "CyclopsGroup", 1295380000},
+				{"../../svnToGitRepos/eclipse.jdt.core", "JDTCore", 1316790694},
+				{"../../svnToGitRepos/eclipse.jdt.debug", "JDTDebug", 1316538989},
+				{"../../svnToGitRepos/eclipse.jdt.ui", "JDTUI", 1316608366},
+				{"../../svnToGitRepos/eclipse.platform", "EclipsePlatform", 1317272400},
+				{"../../svnToGitRepos/eclipse.platform.common", "EclipsePlatformCommon", 1318741200},
+				{"../../svnToGitRepos/eclipse.platform.debug", "EclipsePlatformDebug", 1316408400},
+				{"../../svnToGitRepos/eclipse.platform.team", "EclipsePlatformTeam", 1316581200},
+				{"../../svnToGitRepos/eclipse.platform.text", "EclipsePlatformText", 1316149200},
+				{"../../svnToGitRepos/FFmpeg", "FFmpeg", 1295258573},
+				{"../../svnToGitRepos/libreoffice", "LibreOffice", 1282256340},
+				{"../../svnToGitRepos/MPS", "MPS", 1277726186},
+				{"../../svnToGitRepos/prototype", "prototype", 1205935492},
+				{"../../svnToGitRepos/thymeleaf", "Thymeleaf", 1337530081},
+				{"../../svnToGitRepos/upm-swing/", "UPM", 1287344636}
+		};
+		
+		return Arrays.asList(data);
 	}
 	
 	@Test
 	public void test() throws Exception {
-		new SvnToGitRepoAnalysisLauncher().run();
+		launcher.run();
 	}
 }
