5690b9515777193ba6c7c0f3fa9e5117c0785201
Caius Brindescu <brind@illinois.edu>
2013-08-02 14:16:37 -0500

    Added logic to restart the analysis at a given point

--- infra/src/edu/illinois/gitsvn/analysis/launchers/RecoverableLauncher.java
+++ infra/src/edu/illinois/gitsvn/analysis/launchers/RecoverableLauncher.java
@@ -1,37 +1,35 @@
 package edu.illinois.gitsvn.analysis.launchers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import edu.illinois.gitsvn.analysis.SpecificCommitStartCutoffAnalysis;
 import edu.illinois.gitsvn.infra.AnalysisConfiguration;
 import edu.illinois.gitsvn.infra.AnalysisLauncher;
 
 public class RecoverableLauncher extends AnalysisLauncher {
-	
+
 	public static final String REPO_PATH_PROPERTY = "edu.illinois.gitsvn.repopath";
+	public static final String PROJECT_NAME_PROPERTY = "edu.illinois.gitsvn.projectname";
 	public static final String COMMIT_IDS_PROPERTY = "edu.illinois.gitsvn.commitids";
 	public static final String CUTOFF_PROPERTY = "edu.illinois.gitsvn.cutoff";
-	
-	private String repoPath;
-	private List<String> commitIDs;
-	private long cutoffTime;
-	
+
 	@Override
 	public void run() throws Exception {
 		ArrayList<AnalysisConfiguration> configurations = new ArrayList<AnalysisConfiguration>();
-		populateWithConfigurations(configurations);
-		
-		loadRecoverPoint();
-	}
 
-	private void loadRecoverPoint() {
-		// TODO Auto-generated method stub
-		
+		populateWithConfigurations(configurations);
 	}
 
 	@Override
 	protected void populateWithConfigurations(List<AnalysisConfiguration> configurations) {
-		//magic loading goes here
+		String repoPath = System.getProperty(REPO_PATH_PROPERTY);
+		String projectName = System.getProperty(PROJECT_NAME_PROPERTY);
+		List<String> commitIDs = Arrays.asList(System.getProperty(COMMIT_IDS_PROPERTY).split(";"));
+		int cutoffTime = Integer.parseInt(System.getProperty(CUTOFF_PROPERTY));
+
+		configurations.add(new SpecificCommitStartCutoffAnalysis(repoPath, projectName, commitIDs, cutoffTime));
 	}
-	
+
 }
