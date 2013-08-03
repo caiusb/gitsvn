69a08e4f21b7fda6cddc632069961c0af1240c59
Caius Brindescu <brind@illinois.edu>
2013-08-02 22:40:45 -0500

    Removed the run method from RecoverableLauncher
    The AnalysisLauncher implementation of run already
    does all I want it to do. I forgot about this detail

--- infra/src/edu/illinois/gitsvn/analysis/launchers/RecoverableLauncher.java
+++ infra/src/edu/illinois/gitsvn/analysis/launchers/RecoverableLauncher.java
@@ -1,6 +1,5 @@
 package edu.illinois.gitsvn.analysis.launchers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,13 +13,6 @@ public class RecoverableLauncher extends AnalysisLauncher {
 	public static final String PROJECT_NAME_PROPERTY = "edu.illinois.gitsvn.projectname";
 	public static final String COMMIT_IDS_PROPERTY = "edu.illinois.gitsvn.commitids";
 	public static final String CUTOFF_PROPERTY = "edu.illinois.gitsvn.cutoff";
-
-	@Override
-	public void run() throws Exception {
-		ArrayList<AnalysisConfiguration> configurations = new ArrayList<AnalysisConfiguration>();
-
-		populateWithConfigurations(configurations);
-	}
 
 	@Override
 	protected void populateWithConfigurations(List<AnalysisConfiguration> configurations) {
