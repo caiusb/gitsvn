d5556b244424d96579cc8989a178a4b6d4ea1de9
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-09-25 13:19:18 -0700

    Added an ASTNode data collector
    And a test to go with it

---
+++ infra/src/edu/illinois/gitsvn/infra/collectors/ASTNodeCollector.java
@@ -0,0 +1,98 @@
+package edu.illinois.gitsvn.infra.collectors;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
+import org.eclipse.jgit.diff.DiffEntry.Side;
+import org.eclipse.jgit.lib.AbbreviatedObjectId;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.AndTreeFilter;
+import org.eclipse.jgit.treewalk.filter.TreeFilter;
+import org.gitective.core.BlobUtils;
+import org.gitective.core.filter.commit.DiffCountFilter;
+
+import edu.illinois.gitsvn.infra.DataCollector;
+import edu.illinois.gitsvn.infra.filters.blacklister.NonSourceCodeFileExtensionBlacklister;
+import fr.labri.gumtree.actions.ActionGenerator;
+import fr.labri.gumtree.actions.model.Action;
+import fr.labri.gumtree.gen.jdt.JdtTreeGenerator;
+import fr.labri.gumtree.matchers.Matcher;
+import fr.labri.gumtree.matchers.MatcherFactories;
+import fr.labri.gumtree.tree.Tree;
+
+public class ASTNodeCollector extends DiffCountFilter implements DataCollector {
+	
+	private int data;
+
+	@Override
+	protected TreeWalk createTreeWalk(RevWalk walker, RevCommit commit) {
+		TreeWalk treeWalk = super.createTreeWalk(walker, commit);
+		TreeFilter filter = AndTreeFilter.create(treeWalk.getFilter(), new NonSourceCodeFileExtensionBlacklister(NonSourceCodeFileExtensionBlacklister.JAVA_EXTENSIONS));
+		treeWalk.setFilter(filter);
+		return treeWalk;
+	}
+	
+	@Override
+	public boolean include(RevCommit commit, Collection<DiffEntry> diffs, int diffCount) {
+		for (DiffEntry diff : diffs) {
+			ChangeType changeType = diff.getChangeType();
+			String oldFileContent = "";
+			String newFileContent = "";
+			
+			switch (changeType) {
+			case MODIFY:
+				AbbreviatedObjectId oldObject = diff.getId(Side.OLD);
+				oldFileContent = BlobUtils.getContent(repository, oldObject.toObjectId());
+			case ADD:
+				AbbreviatedObjectId newObject = diff.getId(Side.NEW);
+				newFileContent = BlobUtils.getContent(repository, newObject.toObjectId());
+				break;
+			}
+			
+			File oldContentFile = new File("A" + System.currentTimeMillis() + ".java");
+			File newContentFile = new File("B"+ System.currentTimeMillis() + ".java");
+			
+			try {
+				oldContentFile.createNewFile();
+				newContentFile.createNewFile();
+				Files.write(oldContentFile.toPath(), oldFileContent.getBytes(), StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+				Files.write(newContentFile.toPath(), newFileContent.getBytes(), StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+				
+				JdtTreeGenerator treeGenerator = new JdtTreeGenerator();
+				Tree oldTree = treeGenerator.fromFile(oldContentFile.getAbsolutePath());
+				Tree newTree = treeGenerator.fromFile(newContentFile.getAbsolutePath());
+				Matcher matcher = MatcherFactories.newMatcher(oldTree, newTree);
+				matcher.match();
+				ActionGenerator actionGenerator = new ActionGenerator(oldTree, newTree, matcher.getMappings());
+				actionGenerator.generate();
+				List<Action> actions = actionGenerator.getActions();
+				data = actions.size();
+			} catch (IOException e) {
+			}
+			
+			oldContentFile.delete();
+			newContentFile.delete();
+			
+		}
+		return true;
+	}
+
+	@Override
+	public String name() {
+		return "AST Nodes changed";
+	}
+
+	@Override
+	public String getDataForCommit() {
+		return "" + data;
+	}
+
+}
---
+++ infra/test/edu/illinois/gitsvn/infra/collectors/ASTNodeCollectorTest.java
@@ -0,0 +1,39 @@
+package edu.illinois.gitsvn.infra.collectors;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ASTNodeCollectorTest extends DataCollectorTestCase {
+	
+	@Before
+	public void before() throws IOException {
+		initTest(new ASTNodeCollector());
+	}
+	
+	@Test
+	public void testAddClass() throws Exception {
+		add("F1.java", "public class X{}", "Adding a class");
+		finder.find();
+		assertEquals("3; ",collector.data);
+	}
+	
+	@Test
+	public void testChangeTypeName() throws Exception {
+		add("F1.java", "public class X{}");
+		add("F1.java", "public class A{}");
+		finder.find();
+		
+		assertEquals("1; 3; ", collector.data);
+	}
+	
+	@Test
+	public void testAddTwoMethods() throws Exception {
+		add("F1.java", "public class F1{}");
+		add("F1.java", "public class F1{void m(){} void n(){}}");
+		finder.find();
+		
+		assertEquals("8; 3; ", collector.data);
+	}
+}
